137478acb17e86b7da4f45ad5c04ed0732e8b177
Seblee <ceeu@vip.qq.com>
2022-07-28 12:06:33 +0800

    add u1 config

---
+++ MDK-ARM/ARC01X1Cube/rtx_conf_cm.d
@@ -0,0 +1,5 @@
+arc01x1cube\rtx_conf_cm.o: RTE\CMSIS\RTX_Conf_CM.c
+arc01x1cube\rtx_conf_cm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\RTOS\RTX\INC\cmsis_os.h
+arc01x1cube\rtx_conf_cm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+arc01x1cube\rtx_conf_cm.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+arc01x1cube\rtx_conf_cm.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\RTOS\RTX\INC\RTX_CM_lib.h
---
+++ MDK-ARM/ARC01X1Cube/startup_stm32f103xe.d
@@ -0,0 +1,1 @@
+arc01x1cube\startup_stm32f103xe.o: startup_stm32f103xe.s
